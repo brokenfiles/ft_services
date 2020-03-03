854bffbb38a1a52e13b689923d138f38bd00a87f
brokenfiles <louis.nevysup@gmail.com>
2020-03-03 23:17:03 +0100

    Adding files

--- setup.sh
+++ setup.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
-docker build -t custom-nginx:1.11 ./srcs/nginx
-
 #check if minikube is not already start and the installations
 minikube start --vm-driver=virtualbox \
         --cpus 3 --disk-size=30000mb --memory=3000mb \
         --bootstrapper=kubeadm
 
+MINIKUBE_IP=$(minikube ip)
+
+eval $(minikube docker-env)
+docker build -t custom-nginx:1.11 srcs/nginx
+
 #adding .yaml
-kubectl apply -f srcs/nginx.yaml
 kubectl apply -f srcs/ingress.yaml
+kubectl apply -f srcs/nginx.yaml
+
+printf "=> minikube ip = %s" $MINIKUBE_IP
+
 
