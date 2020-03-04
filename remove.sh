b73debdd83fa65dc7124f233ed928a7bf218f10f
Brokenfiles <louis.nevysup@gmail.com>
2020-03-04 18:16:54 +0100

    tests.

---
+++ remove.sh
@@ -0,0 +1,5 @@
+kubectl delete -f srcs/ingress.yaml
+kubectl delete -f srcs/wordpress.yaml
+kubectl delete -f srcs/phpmyadmin.yaml
+kubectl delete -f srcs/mysql.yaml
+kubectl delete -f srcs/mysql.yaml
--- setup.sh
+++ setup.sh
@@ -9,12 +9,23 @@ MINIKUBE_IP=$(minikube ip)
 
 eval $(minikube docker-env)
 docker build -t custom-nginx:1.11 srcs/nginx
+docker build -t custom-mysql:1.11  srcs/mysql
+docker build -t custom-ftps:1.6 srcs/ftps
+docker build -t custom-phpmyadmin:1.1 srcs/phpmyadmin
+docker build -t custom-wordpress:1.9 srcs/wordpress
+
+#enable addons
+minikube addons enable ingress
 
 #adding .yaml
-minikube addons enable ingress
-kubectl apply -f srcs/ingress.yaml
-kubectl apply -f srcs/nginx.yaml
+kubectl apply -k srcs
+#kubectl apply -f srcs/ingress.yaml
+#kubectl apply -f srcs/nginx.yaml
+#kubectl apply -f srcs/ftps-deployment.yaml
+#kubectl apply -f srcs/mysql.yaml
+#kubectl apply -f srcs/kubernetes-dashboard.yaml
 
-printf "=> minikube ip = %s" $MINIKUBE_IP
+printf "📦 minikube ip : %s (copied to your clipboard)\n" "$MINIKUBE_IP"
+echo "$MINIKUBE_IP" | pbcopy
 
 
