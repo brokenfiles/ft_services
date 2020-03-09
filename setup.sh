f5472a2b72a25ca17e97c661f209142bb05efd7e
Brokenfiles <louis.nevysup@gmail.com>
2020-03-09 13:35:42 +0100

    Adding files

--- setup.sh
+++ setup.sh
@@ -1,31 +1,43 @@
 #!/bin/sh
 
 #check if minikube is not already start and the installations
-minikube start --vm-driver=virtualbox \
-        --cpus 3 --disk-size=30000mb --memory=3000mb \
-        --bootstrapper=kubeadm
+if ! minikube status >/dev/null 2>&1
+then
+    printf "Starting minikube...\n"
+    if ! minikube start --vm-driver=virtualbox --cpus 3 --disk-size=30000mb --memory=3000mb --bootstrapper=kubeadm
+    then
+        printf "Minikube cannot start !\n"
+        exit 1
+    fi
+    minikube addons enable metrics-server
+    minikube addons enable ingress
+fi
 
-MINIKUBE_IP=$(minikube ip)
+IP_ADDRESS=$(minikube ip)
 
 eval $(minikube docker-env)
-docker build -t custom-nginx:1.11 srcs/nginx
-docker build -t custom-mysql:1.11  srcs/mysql
-docker build -t custom-ftps:1.6 srcs/ftps
-docker build -t custom-phpmyadmin:1.1 srcs/phpmyadmin
-docker build -t custom-wordpress:1.9 srcs/wordpress
-
-#enable addons
-minikube addons enable ingress
+docker build -t my-nginx:1.11 srcs/nginx
+docker build -t my-mysql:1.11  srcs/mysql
+docker build -t my-ftps:1.6 srcs/ftps
+docker build -t my-phpmyadmin:1.1 srcs/phpmyadmin
+docker build -t my-wordpress:1.9 srcs/wordpress
 
 #adding .yaml
-kubectl apply -k srcs
-#kubectl apply -f srcs/ingress.yaml
-#kubectl apply -f srcs/nginx.yaml
-#kubectl apply -f srcs/ftps-deployment.yaml
-#kubectl apply -f srcs/mysql.yaml
-#kubectl apply -f srcs/kubernetes-dashboard.yaml
+kubectl apply -f srcs/ingress.yaml
+kubectl apply -f srcs/wordpress.yaml
+kubectl apply -f srcs/nginx.yaml
+kubectl apply -f srcs/ftps-deployment.yaml
+kubectl apply -f srcs/mysql.yaml
+kubectl apply -f srcs/phpmyadmin.yaml
+kubectl apply -f srcs/kubernetes-dashboard.yaml
 
-printf "📦 minikube ip : %s (copied to your clipboard)\n" "$MINIKUBE_IP"
-echo "$MINIKUBE_IP" | pbcopy
+printf "📦 minikube ip : %s\n" "$IP_ADDRESS"
+while true; do
+    read -p "Copy [$IP_ADDRESS] to your clipboard ? (y/n) " yn
+    case $yn in
+        [Yy]* ) echo "$IP_ADDRESS" | pbcopy; printf "%s copied to your clipboard.\n" "$IP_ADDRESS"; break;;
+        * ) exit;;
+    esac
+done
 
 
